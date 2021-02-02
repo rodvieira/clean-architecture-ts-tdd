0a6b83991e3daa600702956f89ab3db448ab1cec
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-02-01 22:59:14 -0300

    Adicionando testes de deletar o cache e salvar o cache

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,17 +1,33 @@
 class LocalSavePurchases {
-  constructor (private readonly cacheStore: CacheStore) {}
+  constructor(private readonly cacheStore: CacheStore) {}
+
+  async save(): Promise<void> {
+    this.cacheStore.delete();
+  }
 }
 interface CacheStore {
+  delete: () => void;
 }
 
 class CacheStoreSpy implements CacheStore {
-  deleteCallsCount = 0
+  deleteCallsCount = 0;
+
+  delete(): void {
+    this.deleteCallsCount++;
+  }
 }
 
-describe('LocalSavePurchases', () => {
-  test('Should not delete cache on sut.init', () => {
-    const cacheStore = new CacheStoreSpy()
-    new LocalSavePurchases(cacheStore)
+describe("LocalSavePurchases", () => {
+  test("Should not delete cache on sut.init", () => {
+    const cacheStore = new CacheStoreSpy();
+    new LocalSavePurchases(cacheStore);
     expect(cacheStore.deleteCallsCount).toBe(0);
-  })
-})
+  });
+
+  test("Should delete old cache on sut.save", async () => {
+    const cacheStore = new CacheStoreSpy();
+    const sut = new LocalSavePurchases(cacheStore);
+    await sut.save();
+    expect(cacheStore.deleteCallsCount).toBe(1);
+  });
+});
