ff52353129db10612c5d80aae61ac1a99f8db545
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-02-03 23:02:26 -0300

    ensure LocalSavePurchases insert correct data on Cache

--- src/domain/usecases/save-purchases.ts
+++ src/domain/usecases/save-purchases.ts
@@ -1,11 +1,11 @@
 export interface SavePurchases {
-  save: (purchases: Array<SavePurchases.Params>) => Promise<void>
+  save: (purchases: Array<SavePurchases.Params>) => Promise<void>;
 }
 
-namespace SavePurchases {
+export namespace SavePurchases {
   export type Params = {
-    id: string
-    date: Date
-    value: number
-  }
+    id: string;
+    date: Date;
+    value: number;
+  };
 }
