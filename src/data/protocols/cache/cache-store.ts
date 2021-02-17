d362d94b8e49fb7a2f000ef21f22ca7783cbdec5
Rodrigo Vieira <rodrigodenizads@gmail.com>
2021-02-16 22:03:01 -0300

    ensure LocalLoadPurchases returns a list of purchases if cache is less 3 days old

--- src/data/protocols/cache/cache-store.ts
+++ src/data/protocols/cache/cache-store.ts
@@ -1,6 +1,6 @@
 export interface CacheStore {
   delete: (key: string) => void;
-  fetch: (key: string) => void;
+  fetch: (key: string) => any;
   insert: (key: string, value: any) => void;
   replace: (key: string, value: any) => void;
 }
